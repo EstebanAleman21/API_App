91db3a72f2442018bfb4660941b656d86210010e
Esteban Aleman <esteban21112002@gmail.com>
2024-04-18 17:35:59 -0600

    Initial Commit

---
+++ API/APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIApp.swift
+//  API
+//
+//  Created by Esteban Aleman on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API
+//
+//  Created by Esteban Aleman on 18/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
